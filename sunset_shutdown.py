d042a4a7d27ee049fde16c0a22c46372bf42f8ef
MarkNoordraven <mn@disroot.org>
2019-08-06 20:44:52 +0200

    added while loop to keep running

--- sunset_shutdown.py
+++ sunset_shutdown.py
@@ -1,5 +1,42 @@
-import pandas as pd
-from tabula import read_pdf
+#!/usr/bin/env python
+# coding: utf-8
 
-import beautifulsoup
-df = read_pdf('tijden_van_zonopkomst_en_-ondergang_2019.pdf.pdf')
+# In[ ]:
+
+
+from bs4 import BeautifulSoup
+import os
+import time
+import requests
+import pandas as pd
+
+
+# In[ ]:
+
+
+page = requests.get('https://www.timeanddate.com/sun/netherlands/amsterdam')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+sunset = soup.find_all(class_='three')[2].contents[0]
+sunset
+
+
+# In[3]:
+
+
+sunset_datetime = pd.to_datetime(sunset)
+sunset_datetime
+
+
+# In[ ]:
+
+
+starttime=time.time()
+while True:
+    now = pd.to_datetime('today')
+    print(f'now is :[{now}], sunset is: [{sunset_datetime}]')
+    if now > sunset_datetime:
+        os.system("shutdown /s /t 1")
+    time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
