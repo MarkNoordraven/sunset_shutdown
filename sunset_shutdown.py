73fcd0d5bdbc81276c088d0912461e3753337ae5
MarkNoordraven <45014197+MarkNoordraven@users.noreply.github.com>
2021-05-08 08:49:59 +0200

    Add files via upload

--- sunset_shutdown.py
+++ sunset_shutdown.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 from bs4 import BeautifulSoup
@@ -11,32 +11,38 @@ import requests
 import pandas as pd
 
 
-# In[ ]:
+# In[29]:
 
 
 page = requests.get('https://www.timeanddate.com/sun/netherlands/amsterdam')
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
-sunset = soup.find_all(class_='three')[2].contents[0]
+sunset = soup.find_all('p', class_ = 'dn-mob')[0].text.split()[2][:5]
 sunset
 
 
-# In[3]:
+# In[30]:
 
 
 sunset_datetime = pd.to_datetime(sunset)
 sunset_datetime
 
 
-# In[ ]:
+# In[33]:
 
 
 starttime=time.time()
 while True:
     now = pd.to_datetime('today')
-    print(f'now is :[{now}], sunset is: [{sunset_datetime}]')
+    print(f"now is :[{now}], sunset is: [{sunset_datetime}]")
     if now > sunset_datetime:
         os.system("shutdown /s /t 1")
     time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+
+
+# In[ ]:
+
+
+
 
